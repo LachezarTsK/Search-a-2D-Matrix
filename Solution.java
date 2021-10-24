c3ef9d291df5774446c9dfac5e67c52d4f3981bf
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-10-25 00:10:07 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,14 +3,22 @@ public class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        //Search rows.
+        int row = searchRows(matrix, target);
+        if (row < 0) {
+            return false;
+        }
+        int column = searchColums(matrix, target, row);
+        return column >= 0;
+    }
+
+    public int searchRows(int[][] matrix, int target) {
         int up = 0;
         int down = matrix.length - 1;
 
         while (up <= down) {
             int mid = up + (down - up) / 2;
             if (matrix[mid][0] == target) {
-                return true;
+                return mid;
             }
 
             if (matrix[mid][0] < target) {
@@ -19,28 +27,25 @@ public class Solution {
                 down = mid - 1;
             }
         }
+        return down;
+    }
 
-        if (down < 0) {
-            return false;
-        }
-
-        //Search columns.
+    public int searchColums(int[][] matrix, int target, int row) {
         int left = 0;
         int right = matrix[0].length - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (matrix[down][mid] == target) {
-                return true;
+            if (matrix[row][mid] == target) {
+                return mid;
             }
 
-            if (matrix[down][mid] < target) {
+            if (matrix[row][mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-
-        return false;
+        return -1;
     }
 }
