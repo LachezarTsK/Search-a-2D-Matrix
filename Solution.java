e4d903d1757d1e73239409605f89b6d230a05847
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-31 06:46:27 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,51 +1,40 @@
 
 public class Solution {
 
+    int rows;
+    int columns;
+
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        int row = searchRows(matrix, target);
-        if (row < 0) {
-            return false;
-        }
-        int column = searchColums(matrix, target, row);
-        return column >= 0;
-    }
-
-    public int searchRows(int[][] matrix, int target) {
-        int up = 0;
-        int down = matrix.length - 1;
-
-        while (up <= down) {
-            int mid = up + (down - up) / 2;
-            if (matrix[mid][0] == target) {
-                return mid;
-            }
-
-            if (matrix[mid][0] < target) {
-                up = mid + 1;
-            } else {
-                down = mid - 1;
-            }
-        }
-        return down;
-    }
-
-    public int searchColums(int[][] matrix, int target, int row) {
+        rows = matrix.length;
+        columns = matrix[0].length;
         int left = 0;
-        int right = matrix[0].length - 1;
+        int right = rows * columns - 1;
 
         while (left <= right) {
+
             int mid = left + (right - left) / 2;
-            if (matrix[row][mid] == target) {
-                return mid;
+            int row = getRowFromIndexFlattenedMatrix(mid);
+            int column = getColumnFromIndexFlattenedMatrix(mid);
+
+            if (matrix[row][column] == target) {
+                return true;
             }
 
-            if (matrix[row][mid] < target) {
+            if (matrix[row][column] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return -1;
+        return false;
+    }
+
+    private int getRowFromIndexFlattenedMatrix(int index) {
+        return index / columns;
+    }
+
+    private int getColumnFromIndexFlattenedMatrix(int index) {
+        return index % columns;
     }
 }
